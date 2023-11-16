36974f5b72e482180c7eddb2a175c45ea17aca2f
Julio César Alves <julio.caburu@gmail.com>
2023-11-16 14:20:51 -0300

    Versão inicial

---
+++ App.java
@@ -0,0 +1,19 @@
+
+/**
+ * Classe principal do programa, que tem o método main, e que,
+ * portanto, pode ser executada fora do BlueJ.
+ * 
+ * @author Julio César Alves
+ * @version 2023.11.16
+ */
+public class App {
+    /**
+     * Método que inicia o programa.
+     * Crie um objeto da classe 'InterfaceUsuario' e chama seu
+     * método 'executar'.
+     */
+    public static void main(String[] args) throws Exception {
+        InterfaceUsuario iu = new InterfaceUsuario();
+        iu.executar();
+    }
+}
